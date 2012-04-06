5030cea80a55ef2e49cc4fe4f5f78f93f84ab4fe
wallymathieu <gewalli@gmail.com>
2012-04-06 22:32:49 +0200

    Testing out graph traversal.

--- Mejram.Analysis/relations_tests.rb
+++ Mejram.Analysis/relations_tests.rb
@@ -2,14 +2,18 @@ $:.unshift File.dirname(__FILE__)
 require 'relations'
 require 'test/unit'
 
+require 'rgl/traversal'
+
 class RelationsTest < Test::Unit::TestCase
     def setup
         @content = File.open('sakila.Tables.json.txt').read 
         @rel = Relations.new 
     end
 
-    def test_f
+    def test_graph_traversal
         p=@rel.parse_json_text(@content)
-        puts p
+        puts p.bfs_search_tree_from(p.detect{ |x| 
+            x.methods; x == 'store' 
+        })
     end
 end
