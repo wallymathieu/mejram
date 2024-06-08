2a9f15b4164f4e756767b1d8d7c70d85f6aadf8e
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-06-08 11:17:28 +0300

    not yet done, rewriting docker compose

--- mssql/import-data.sh
+++ mssql/import-data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#!/bin/bash
-#sleep 10s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -d master -i /db-import/sql-server-sakila-schema.sql
+/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -d master -P ${SA_PASSWORD} -i /db-import/sql-server-sakila-schema.sql
 
 #import the data
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -d sakila -i /db-import/sql-server-sakila-insert-data.sql
+/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -d sakila -P ${SA_PASSWORD} -i /db-import/sql-server-sakila-insert-data.sql
