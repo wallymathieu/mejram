c1c7d789ffbcf49812259c3927be9112b56d11a1
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-06-09 18:00:44 +0300

    outside script to import data

--- pg/import-data.sh
+++ pg/import-data.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# make sure to install psql (PostgreSQL client)
+pushd $(dirname "${0}") > /dev/null
+cd ../
+PG_HOST=localhost
+PG_PORT=5434
+source .env
+
 set -e
-psql -v ON_ERROR_STOP=1 <<-EOSQL
+PGPASSWORD=$POSTGRES_PASSWORD psql -h $PG_HOST -p $PG_PORT -U postgres -v ON_ERROR_STOP=1 <<-EOSQL
 	CREATE USER sakila;
 	CREATE DATABASE sakila;
 	GRANT ALL PRIVILEGES ON DATABASE sakila TO sakila;
 EOSQL
-psql -U postgres -d sakila -f /db-import/postgres-sakila-schema.sql
-psql -U postgres -d sakila -f /db-import/postgres-sakila-insert-data.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -h $PG_HOST -p $PG_PORT -U postgres -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-schema.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -h $PG_HOST -p $PG_PORT -U postgres -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-insert-data.sql
