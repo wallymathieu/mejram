39d833cf73e30f7458388c7abc5b9facb80bc2d7
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-06-10 23:17:21 +0300

    Write many to many

--- csharp/Mejram.Console/DotGraphController.cs
+++ csharp/Mejram.Console/DotGraphController.cs
@@ -20,6 +20,20 @@ namespace Mejram
 			dotGraphGenerator.WriteDot(dot);
 		}
 
+		/// <summary>
+	    /// Writes dot file and send to dot
+		/// Writes out connections between tables that has many as many as direct connections.
+	    /// </summary>
+	    /// <param name="dot">dot tool</param>
+	    /// <param name="tablesPath">Tables json file path.</param>
+	    public void WriteDotMany (string dot,
+            string tablesPath = "outfile.tables.json")
+		{
+            var serialization = new Serialization(tablesPath);
+		    dotGraphGenerator.GenerateDotFileWithLessManyToMany(serialization.Deserialize());
+			dotGraphGenerator.WriteDot(dot);
+		}
+
 	    /// <summary>
 	    /// Writes dot file and send to neato
 	    /// </summary>
--- csharp/Mejram.Console/Infrastructure/DotGraphGenerator.cs
+++ csharp/Mejram.Console/Infrastructure/DotGraphGenerator.cs
@@ -8,52 +8,75 @@ using Mejram.Models;
 namespace Mejram
 {
     internal class DotGraphGenerator(
-        string @out= "outfile.png",
-        string outNeato = "outfile.neato.png",
         string dotfile = "outfile.dot")
     {
-
         public void GenerateDotFile(ICollection<Table> tables)
         {
-            using (var m = File.Open(dotfile, FileMode.Create))
-            using (var sout = new StreamWriter(m))
-            {
-                //size=""90,90""; 
-                sout.WriteLine(@"digraph graphname { 
+            using var m = File.Open(dotfile, FileMode.Create);
+            using var sout = new StreamWriter(m);
+            //size=""90,90""; 
+            sout.WriteLine(@"digraph graphname { 
 #ratio = square;
 node [style=filled];
 hexagon [style=bold,style=filled];");
-                var conventions= Analysis.TableNameConventions.Default();
-                var manyToMany = Analysis.ProbableManyToManyTables(tables, conventions).Where(t=>t.HasMatchingOutgoingForeignKeys());
-                var manyToManyNames = manyToMany.Select(t=>t.Table.TableName).ToHashSet();
-                bool IsAManyToMany(Table t) => manyToManyNames.Contains(t.TableName);
-                foreach (var table in tables.Where(t=>!IsAManyToMany(t)))
-                {
-                    //,style=filled
-                    sout.WriteLine("\"{0}\" {1};",
-                        table.TableName, table.HasPrimalKey() ? "[shape=hexagon]" : "");
-                }
-                foreach (var (fromTable, toTable) in (
-                                 from mm in manyToMany
-                                 select (mm.FirstOtherTable, mm.SecondOtherTable)).Distinct())
-                {
-                    sout.WriteLine("\"{0}\" <-> \"{1}\" ;", fromTable, toTable);
-                }
-                foreach (var (fromTable, columnName, toTable) in (
-                                 from table in tables
-                                 where !IsAManyToMany(table)
-                                 from fk in table.ForeignKeys
-                                 from fkcol in fk.Columns
-                                 select (fkcol.From.TableName, fkcol.From.ColumnName, fkcol.To.TableName)).Distinct())
-                {
-                    sout.WriteLine("\"{0}\" -> \"{1}\"  [label=\"{2}\"];", fromTable, toTable,
-                                   columnName);
-                }
-                sout.WriteLine("}");
+            var conventions = Analysis.TableNameConventions.Default();
+            foreach (var table in tables)
+            {
+                //,style=filled
+                sout.WriteLine("\"{0}\" {1};",
+                    table.TableName, table.HasPrimalKey() ? "[shape=hexagon]" : "");
             }
+            foreach (var (fromTable, columnName, toTable) in (
+                             from table in tables
+                             from fk in table.ForeignKeys
+                             from fkcol in fk.Columns
+                             select (fkcol.From.TableName, fkcol.From.ColumnName, fkcol.To.TableName)).Distinct())
+            {
+                sout.WriteLine("\"{0}\" -> \"{1}\"  [label=\"{2}\"];", fromTable, toTable,
+                               columnName);
+            }
+            sout.WriteLine("}");
         }
 
-        public void WriteDot(string dotExe)
+        public void GenerateDotFileWithLessManyToMany(ICollection<Table> tables)
+        {
+            using var m = File.Open(dotfile, FileMode.Create);
+            using var sout = new StreamWriter(m);
+            //size=""90,90""; 
+            sout.WriteLine(@"digraph graphname { 
+#ratio = square;
+node [style=filled];
+hexagon [style=bold,style=filled];");
+            var conventions = Analysis.TableNameConventions.Default();
+            var manyToMany = Analysis.ProbableManyToManyTables(tables, conventions).Where(t => t.HasMatchingOutgoingForeignKeys());
+            var manyToManyNames = manyToMany.Select(t => t.Table.TableName).ToHashSet();
+            bool IsAManyToMany(Table t) => manyToManyNames.Contains(t.TableName);
+            foreach (var table in tables.Where(t => !IsAManyToMany(t)))
+            {
+                //,style=filled
+                sout.WriteLine("\"{0}\" {1};",
+                    table.TableName, table.HasPrimalKey() ? "[shape=hexagon]" : "");
+            }
+            foreach (var (fromTable, toTable, connectingTable) in (
+                             from mm in manyToMany
+                             select (mm.FirstOtherTable, mm.SecondOtherTable, mm.Table.TableName)).Distinct())
+            {
+                sout.WriteLine("\"{0}\" -> \"{1}\" [label=\"{2}\"] ; \"{1}\" -> \"{0}\" [label=\"{2}\"];", fromTable, toTable, "*");
+            }
+            foreach (var (fromTable, columnName, toTable) in (
+                             from table in tables
+                             where !IsAManyToMany(table)
+                             from fk in table.ForeignKeys
+                             from fkcol in fk.Columns
+                             select (fkcol.From.TableName, fkcol.From.ColumnName, fkcol.To.TableName)).Distinct())
+            {
+                sout.WriteLine("\"{0}\" -> \"{1}\"  [label=\"{2}\"];", fromTable, toTable,
+                               columnName);
+            }
+            sout.WriteLine("}");
+        }
+
+        public void WriteDot(string dotExe, string @out= "outfile.png")
         {
             var fileName = dotExe;
             var arguments = String.Format("-Tpng {0} -o {1}", dotfile,
@@ -70,7 +93,7 @@ hexagon [style=bold,style=filled];");
             dot.Start();
         }
 
-        public void WriteNeato(string neatoExe)
+        public void WriteNeato(string neatoExe, string outNeato = "outfile.neato.png")
         {
             var arguments = String.Format("-Tpng {0} -o {1}", dotfile,
                                           outNeato);
